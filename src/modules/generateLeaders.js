410feb346c757fa58451b87d1d68a34ec621d1b4
miftah1991 <miftah.amin1991@gmail.com>
2022-03-02 21:17:40 +0430

    Get data from API

--- src/modules/generateLeaders.js
+++ src/modules/generateLeaders.js
@@ -1,16 +1,14 @@
-import Leaders from './leaders.js';
-
-const getData = () => {
+const getData = (data) => {
   const container = document.getElementById('leader-lis');
   container.innerHTML = '';
-  if (localStorage.getItem('leaders') != null) {
-    Leaders.scores = JSON.parse(localStorage.getItem('leaders'));
-    Leaders.scores.forEach((element, index) => {
+  if (data != null) {
+    console.log(data)
+    data.result.forEach((element, index) => {
       const li = document.createElement('li');
       li.className = 'elements';
       const titleDiv = document.createElement('div');
       titleDiv.className = 'leader-title';
-      titleDiv.textContent = `${element.name} - ${element.score}`;
+      titleDiv.textContent = `${element.user} - ${element.score}`;
       li.appendChild(titleDiv);
       container.append(li);
       if (index % 2 !== 0) {
--- src/modules/leaders.js
+++ src/modules/leaders.js
@@ -1,14 +1,20 @@
-class Leaders {
-    static scores = [];
+import getData from './generateLeaders.js';
+  const dataURL = "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/6uj0uqnZpshKtXlAZwzJ/scores"; 
+    const getAPIData = () => {
+      fetch(dataURL)
+        .then((res) => res.json())
+        .then((data) => {
+          getData(data);
+        });
+    };
+    const postData = (data) => {
+      fetch(dataURL, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      });
+    };
 
-    constructor(name, score) {
-      this.name = name;
-      this.score = score;
-    }
-
-    static saveLocalStorage() {
-      localStorage.setItem('leaders', JSON.stringify(this.scores));
-    }
-}
-
-export default Leaders;
+export { getAPIData,postData};
