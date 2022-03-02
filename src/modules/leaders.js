1b3be864acf3dbf855cde4643469e14a9e8fb78d
miftah1991 <miftah.amin1991@gmail.com>
2022-03-02 22:12:09 +0430

    Changed to async

--- src/modules/leaders.js
+++ src/modules/leaders.js
@@ -1,15 +1,13 @@
 import getData from './generateLeaders.js';
 
 const dataURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/6uj0uqnZpshKtXlAZwzJ/scores';
-const getAPIData = () => {
-  fetch(dataURL)
-    .then((res) => res.json())
-    .then((data) => {
-      getData(data);
-    });
+
+const getAPIData = async () => {
+  const response = await (await fetch(dataURL)).json();
+  getData(response);
 };
-const postData = (data) => {
-  fetch(dataURL, {
+const postData = async (data) => {
+  await fetch(dataURL, {
     method: 'POST',
     body: JSON.stringify(data),
     headers: {
