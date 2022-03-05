78acdb1477948ebe918a4480e180372237d53b72
miftah1991 <miftah.amin1991@gmail.com>
2022-03-05 19:15:42 +0430

    Fix linters

--- src/modules/generateLeaders.js
+++ src/modules/generateLeaders.js
@@ -2,8 +2,7 @@ const getData = (data) => {
   const container = document.getElementById('leader-lis');
   container.innerHTML = '';
   if (data != null) {
-    console.log(data.result);
-    let newArray = data.result.sort((a, b) => Number((a.score) > Number(b.score)) ? -1 : 1)
+    const newArray = data.result.sort((a, b) => (Number((a.score) > Number(b.score)) ? -1 : 1));
     newArray.forEach((element, index) => {
       const li = document.createElement('li');
       li.className = 'elements';
