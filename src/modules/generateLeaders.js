fde52a4640bd47102c9a5e49527a74ebbe44647b
miftah1991 <miftah.amin1991@gmail.com>
2022-03-05 19:14:30 +0430

    Sort By Maximum Numbers

--- src/modules/generateLeaders.js
+++ src/modules/generateLeaders.js
@@ -2,7 +2,9 @@ const getData = (data) => {
   const container = document.getElementById('leader-lis');
   container.innerHTML = '';
   if (data != null) {
-    data.result.forEach((element, index) => {
+    console.log(data.result);
+    let newArray = data.result.sort((a, b) => Number((a.score) > Number(b.score)) ? -1 : 1)
+    newArray.forEach((element, index) => {
       const li = document.createElement('li');
       li.className = 'elements';
       const titleDiv = document.createElement('div');
